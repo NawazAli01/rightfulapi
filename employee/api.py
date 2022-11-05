cd487c91610e4b8d6926c9a3cb9f5f930e0eb22d
NawazAli01 <63184410+NawazAli01@users.noreply.github.com>
2022-11-05 10:07:26 +0530

    Update api.py

--- employee/api.py
+++ employee/api.py
@@ -3,20 +3,22 @@ from rest_framework.response import Response
 from .serializer import EmployeeSerializer
 from .models import Employee
 
+# Class is for create api view and query all objects
 class EmployeeCreateApi(generics.CreateAPIView):
     queryset = Employee.objects.all()
     serializer_class = EmployeeSerializer
 
+# Class is for Retrieve apis and query all objects
 class EmployeeApi(generics.ListAPIView):
     queryset = Employee.objects.all()
     serializer_class = EmployeeSerializer
 
-
+# Class is for Update apis data and query all objects
 class EmployeeUpdateApi(generics.RetrieveUpdateAPIView):
     queryset = Employee.objects.all()
     serializer_class = EmployeeSerializer
 
-
+# Class is for Delete request api and query all objects
 class EmployeeDeleteApi(generics.DestroyAPIView):
     queryset = Employee.objects.all()
     serializer_class = EmployeeSerializer
